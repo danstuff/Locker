d3c04809da4716471d540b0838633a91900483a3
Dan <raydongames@gmail.com>
2016-12-19 20:35:17 -0500

    Interface Update pt. 2

--- main.cpp
+++ main.cpp
@@ -1,49 +1,89 @@
 #include "main.h"
 
-void xorencrypt(string filename, string key){
+string xorencrypt(string filein, string fileout, string key, bool delete_in) {
 	//add all of file to a string
-	fstream file(filename, ios::in);
+	fstream file;
+	file.open(filein.c_str(), ios::in);
+
+	if (!file.is_open()) {
+		cout << "[ERROR] File not found: please add \"" << filein
+				<< "\" to continue" << endl;
+	}
 
 	string unencrypted = "";
 	string line = "";
 
-	while(!file.eof()){
+	while (!file.eof()) {
 		getline(file, line);
 		unencrypted += line;
 	}
-	
+
 	file.close();
+
+	if(delete_in){
+		remove(filein.c_str());
+	}
 
 	//encrypt the file
 	string encrypted = "";
 
-	for(uint i = 0; i < unencrypted.length(); i++){
-		encrypted += unencrypted.at(i) ^ key.at(i%key.length());
+	for (uint i = 0; i < unencrypted.length(); i++) {
+		encrypted += unencrypted.at(i) ^ key.at(i % key.length());
 	}
 
-	//replace file with encrypted data
-	file.open(filename, ios::out);
+	//replace out file with encrypted data
+	fstream fout;
+	fout.open(fileout.c_str(), ios::out);
 
-	file << encrypted;
+	if (!fout.is_open()) {
+		cout << "[ERROR] File not found: please add \"" << fileout
+				<< "\" to continue" << endl;
+	}
 
-	file.close();
+	fout << encrypted;
+
+	fout.close();
+
+	return encrypted;
 }
 
-int main(){
-	for(uint i = 0; i < 36; i++)
+int main() {
+	for (uint i = 0; i < 36; i++)
 		cout << char(219);
+
 	cout << " LOCKER ";
-	for(uint i = 0; i < 36; i++)
+
+	for (uint i = 0; i < 36; i++)
 		cout << char(219);
 
-	cout << "Enter Key:" << endl;
-	cout << "> ";
+	while (true) {
+		cout << endl;
 
-	string key;
+		cout << ">";
 
-	cin >> key;
+		string command;
+		cin >> command;
 
-	xorencrypt("locker.txt", key);
+		if (command == "lock") {
+			cout << "Enter Key:";
 
-	return 0;
+			string key;
+			cin >> key;
+
+			xorencrypt("unlock.txt", "locker.dat", key, true);
+			cout << "File locked using key \"" << key << "\"" << endl;
+		} else if (command == "unlock") {
+			cout << "Enter Key:";
+
+			string key;
+			cin >> key;
+
+			cout << xorencrypt("locker.dat", "unlock.txt", key, false) << endl;
+			cout << "File unlocked using key \"" << key << "\"" << endl;
+		} else if (command == "quit") {
+			return 0;
+		}else{
+			cout << "Invalid command \"" << command << ".\" Valid commands are \"lock,\" \"unlock,\" and \"quit.\"" << endl;
+		}
+	}
 }
